631c1dc3f3a6866ef376eefff6d0a71997de71bb
AbdulsispnShakoor <shakoor.sispn@gmail.com>
2024-09-23 22:00:56 +0500

    feat: husky and some other start packagies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("express app")
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+// Routes
+
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 8001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
